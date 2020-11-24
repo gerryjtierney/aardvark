c03c1a13057641f922d972cafa8199bd94ebfee6
gerryjtierney <gerryjtierney@gmail.com>
2020-11-24 10:46:15 +0000

    layout improvements
    Twitter tab now gone, contact form page now no longer jumps around when switching "tabs"

--- mernshoppinglist_111120/client/src/bottomThird/Socials.js
+++ mernshoppinglist_111120/client/src/bottomThird/Socials.js
@@ -8,7 +8,7 @@ class Socials extends Component{
     render(){
         return(
             <div className="Socials">
-                <div className="Socials-twitter">Twitter</div>
+                
                 <div className="Socials-instagram">Instagram</div>
             </div>
         )
