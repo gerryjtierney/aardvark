c1e8df9500b17b824b1f2bd5efd6d25e1a032d5c
gerryjtierney <gerryjtierney@gmail.com>
2020-11-26 17:08:16 +0000

    booking component styling
    mostly changes to booking style, some state changes for React side

--- mernshoppinglist_111120/client/src/components/BookingComponent.js
+++ mernshoppinglist_111120/client/src/components/BookingComponent.js
@@ -2,20 +2,26 @@ import React, { Component } from "react";
 import { Button, Modal, Col, Form, FormGroup, Label, Input, CustomInput } from "reactstrap";
 
 
-class BookingComponent extends Component{
+class BookingComponent extends Component {
 
     state = {
-        booking0900: false,
-        booking1200: false,
-        booking1700: false,
-        toggled: false
+        booking0900: 3,
+        booking1200: 3,
+        booking1700: 3,
+        toggled: false,
+        userEmail: "",
+        userName: "",
+        userComments: "",
+        userSlot: ""
+
+
     }
 
     toggle = () => {
         this.setState({
             toggled: !this.state.toggled
         })
-    } 
+    }
 
 
 
@@ -26,78 +32,78 @@ class BookingComponent extends Component{
 
         return (
 
-            <div className="PrimaryMenuModal" id="BookingComponent">
+            <div>
                 <div onClick={this.toggle} className="menuButton">Book</div>
 
 
-                <Modal isOpen={this.state.toggled} id="menuModal">
-                    <div className="PrimaryMenuModalContainer">
+                <Modal isOpen={this.state.toggled}>
+                    <div id="BookingComponentFormContainer">
 
 
 
                         <div className="menuModalButtonDiv">
-                            <Button color="dark" className="menuModalButton" onClick={this.toggle}>Close</Button>
+                            <Button color="dark" className="BookingModalButton" onClick={this.toggle}>Close</Button>
                         </div>
 
 
-    <Form className="bookingComponentForm">
-      <FormGroup row>
-        <Label for="exampleEmail" sm={1}>Email</Label>
-        <Col sm={10}>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Enter email" />
-        </Col>
-      </FormGroup>
+                        <Form className="bookingComponentForm">
+                            <FormGroup row>
+                                <Label for="exampleEmail" sm={1}>Email</Label>
+                                <Col sm={9}>
+                                    <Input type="email" name="email" placeholder="Enter email" />
+                                </Col>
+                            </FormGroup>
 
-      <FormGroup row>
-        <Label for="exampleEmail" sm={1}>Name</Label>
-        <Col sm={10}>
-          <Input type="text" name="username" id="username" placeholder="Enter name" />
-        </Col>
-      </FormGroup>
-
-
-
-      <FormGroup row>
-        <Label for="exampleText" sm={1}>Comments</Label>
-        <Col sm={10}>
-          <Input type="textarea" name="text" id="exampleText" />
-        </Col>
-      </FormGroup>
+                            <FormGroup row>
+                                <Label sm={1}>Name</Label>
+                                <Col sm={9}>
+                                    <Input type="text" name="username" id="username" placeholder="Enter name" />
+                                </Col>
+                            </FormGroup>
 
 
 
-      <FormGroup>
-        <Label for="exampleCustomSelect" >Choose your slot</Label>
-        <CustomInput type="select" id="exampleCustomSelect" name="customSelect" className="bookingComponentFormSlotSelect">
-          <option value="">-none-</option>
-          <option>Breakfast</option>
-          <option>Lunch</option>
-          <option>Dinner</option>
-
-        </CustomInput>
-      </FormGroup>
+                            <FormGroup row>
+                                <Label for="exampleText" sm={1}>Comments</Label>
+                                <Col sm={9}>
+                                    <Input type="textarea" name="text" id="exampleText" />
+                                </Col>
+                            </FormGroup>
 
 
 
-      <FormGroup row>
-        <Label for="checkbox2" sm={2}>I agree to the terms and conditions</Label>
-        <Col sm={{ size: 10 }}>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox" id="checkbox2" required/>{' '}
-              
-            </Label>
-          </FormGroup>
-        </Col>
-      </FormGroup>
-      <FormGroup check row>
-        <Col sm={{ size: 10, offset: 2 }}>
-          <Button>Submit</Button>
-        </Col>
-      </FormGroup>
-    </Form>
-                        
-                        
+                            <FormGroup row>
+                                <Label for="exampleCustomSelect" id="slotLabel">Your slot</Label>
+                                <CustomInput type="select" id="exampleCustomSelect" name="customSelect" className="bookingComponentFormSlotSelect">
+                                    <option value="">-none-</option>
+                                    <option>Breakfast</option>
+                                    <option>Lunch</option>
+                                    <option>Dinner</option>
+
+                                </CustomInput>
+                            </FormGroup>
+
+
+
+                            <FormGroup row>
+                                <Label for="checkbox2" sm={2}>Confirm</Label>
+                                <Col sm={{ size: 15 }}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" id="checkbox2" required />{' '}
+
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup check row>
+                                <Col sm={{ size: 10, offset: 2 }}>
+                                    <Button className="BookingSubmitButton">Submit</Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+
+
 
 
 
