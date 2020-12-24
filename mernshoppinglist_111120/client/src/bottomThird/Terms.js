53a1f7f461876df2e459cbfb894f718ca2e0f1a1
gerryjtierney <gerryjtierney@gmail.com>
2020-12-24 17:21:54 +0000

    Update Terms.js
    added link to my github, will change to coming portfolio page

--- mernshoppinglist_111120/client/src/bottomThird/Terms.js
+++ mernshoppinglist_111120/client/src/bottomThird/Terms.js
@@ -9,7 +9,7 @@ class Terms extends Component{
                 <div className="Terms-cookies">Cookies</div>
                 <div className="Terms-conditions">Conditions</div>
                 <div className="Terms-privacy">Privacy</div>
-                <div className="Terms-info">2020 Katie's Place | Site by Tierney Web Development</div>
+                <div className="Terms-info" > <a href="https://www.github.com/gerryjtierney">2020 Katie's Place | Site by Tierney Web Development</a></div>
             </div>
         )
     }
