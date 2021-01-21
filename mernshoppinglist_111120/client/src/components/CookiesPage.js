27f37f8daeab683b4affea1a880db1116143a4df
gerryjtierney <gerryjtierney@gmail.com>
2021-01-21 13:24:11 +0000

    a bit of pruning
    removed a few "spare" images to cut down total slug size

--- mernshoppinglist_111120/client/src/components/CookiesPage.js
+++ mernshoppinglist_111120/client/src/components/CookiesPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "../../src/CookiesPage.css"
 import CookiesTopThird from "../topThird/CookiesTopThird"
 import CookiesMiddleThird from "../middleThird/CookiesMiddleThird"
