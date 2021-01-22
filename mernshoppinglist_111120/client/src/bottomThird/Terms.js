801289f04165b68c94671055e9def5809a88ec66
gerryjtierney <gerryjtierney@gmail.com>
2021-01-22 20:32:55 +0000

    Update Terms.js
    link added

--- mernshoppinglist_111120/client/src/bottomThird/Terms.js
+++ mernshoppinglist_111120/client/src/bottomThird/Terms.js
@@ -17,7 +17,7 @@ class Terms extends Component{
             <div className="Terms">
                 <div className="Terms-cookies" onClick={this.scrollUp}><Link to="/cookies"> Cookies </Link></div>
                 <div className="Terms-conditions" onClick={this.scrollUp}><Link to="/terms">Conditions</Link></div>
-                <div className="Terms-privacy">Privacy</div>
+                <div className="Terms-privacy" onClick={this.scrollUp}><Link to="/privacy">Privacy</Link></div>
                 <div className="Terms-info" > <a href="https://www.github.com/gerryjtierney">2020 Katie's Place | Site by Tierney Web Development</a></div>
             </div>
         )
