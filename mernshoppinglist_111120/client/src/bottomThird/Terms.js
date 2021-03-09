ca198e87cfa38f55f0f9b5c3a802599f69c28fd3
gerryjtierney <gerryjtierney@gmail.com>
2021-03-09 15:50:12 +0000

    link changes

--- mernshoppinglist_111120/client/src/bottomThird/Terms.js
+++ mernshoppinglist_111120/client/src/bottomThird/Terms.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 //import CookiesComponent from "../CookiesComponent"
 
-class Terms extends Component{
+class Terms extends Component {
 
 
-    scrollUp = () =>{
+    scrollUp = () => {
         window.scrollTo(500, 0);
     }
 
-    render(){
+    render() {
 
 
 
-        return(
+        return (
             <div className="Terms">
                 <div className="Terms-cookies" onClick={this.scrollUp}><Link to="/cookies"> Cookies </Link></div>
                 <div className="Terms-conditions" onClick={this.scrollUp}><Link to="/terms">Conditions</Link></div>
                 <div className="Terms-privacy" onClick={this.scrollUp}><Link to="/privacy">Privacy</Link></div>
-                <div className="Terms-info" > <a href="https://www.github.com/gerryjtierney">2020 Katie's Place | Site by Tierney Web Development</a></div>
+                <div className="Terms-info" > <a href="https://www.tierneywebdevelopment">2020 Katie's Place | Site by Tierney Web Development</a></div>
             </div>
         )
     }
---
+++ mernshoppinglist_111120/client/src/components/Switches.js
@@ -0,0 +1,20 @@
+
+
+
+
+const Switches = () => {
+
+
+
+    return ( 
+
+
+
+
+
+
+
+     );
+}
+ 
+export default Switches;
